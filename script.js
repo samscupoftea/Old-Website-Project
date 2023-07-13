0ee402bd87c20cb6379ee3fb004daab4c324fd1a
sam hockaday <samuel.hockaday@gmail.com>
2023-07-13 16:52:03 +0100

    Made variables for each button and text space.

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const answers = [{
     answer1: `"Mindfulness is the aware, balanced acceptance of the present experience. It isn't more complicated than that. It is opening to or receiving the present moment, pleasant or unpleasant, just as it is, without either clinging to it or rejecting it."`,
-    picture: 
+    picture: placeholder,
 }, {
     answer2: `"Mindfulness is the aware, balanced acceptance of the present experience. It isn't more complicated than that. It is opening to or receiving the present moment, pleasant or unpleasant, just as it is, without either clinging to it or rejecting it."`,
-    picture: 
+    picture: placeholder,
 }, {
     answer3: `"The practice of mindfulness allows us to become aware of the stream of thoughts and feelings that we usually identify with as our self."`,
-    picture:
+    picture: placeholder,
 }, {
     answer4: `"You can’t control how you feel. But you can always choose how you act."`,
-    picture:
+    picture: placeholder,
 }, {
     answer5: `"Awakening is not changing who you are, but discarding who you are not."`,
-    picture:
+    picture: placeholder,
 }, {
     answer6: `"Mindfulness is the key to transforming your relationship with life."`,
-    picture: 
+    picture: placeholder,
 },
 
 ]
 
 //Use query selector to target the the button. Then the quote and writer part so that the array text content can replace. 
-let btn = document.querySelector("#Qbtn");
-let btn = document.querySelector("#Qbtn2");
-let btn = document.querySelector("#Qbtn3");
-let btn = document.querySelector("#Qbtn4");
-let btn = document.querySelector("#Qbtn5");
-let btn = document.querySelector("#Qbtn6");
+let qbtn1 = document.querySelector("#Qbt1");
+let qbtn2 = document.querySelector("#Qbtn2");
+let qbtn3 = document.querySelector("#Qbtn3");
+let qbtn4 = document.querySelector("#Qbtn4");
+let qbtn5 = document.querySelector("#Qbtn5");
+let qbtn6 = document.querySelector("#Qbtn6");
 let answer1 = document.querySelector(".answer1");
 let answer2 = document.querySelector(".answer2");
 let answer3 = document.querySelector(".answer3");
